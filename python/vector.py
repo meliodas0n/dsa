5a9cd2043b901cd38958808d1439cc397ead7579
meliodas0n <mrunalnarayana@gmail.com>
2024-03-12 21:52:38 +0530

    i think this is vector

--- python/vector.py
+++ python/vector.py
@@ -4,3 +4,26 @@ class Vector:
 
   def __len__(self):
     return len(self.__coords)
+
+  def __getitem__(self, j):
+    return self.__cords[j]
+
+  def __setitem__(self, j, val):
+    self.__coords[j] = val
+
+  def __add__(self, other):
+    if len(self) != len(other):
+      raise ValueError("Dimensions must agree")
+    result = Vector(len(self))
+    for j in range(len(self)):
+      result[j] = self[j] + other[j]
+    return result
+  
+  def __eq__(self, other):
+    return self.__coord == other.__coords
+
+  def __ne__(self, other):
+    return not self == other
+
+  def __str__(self):
+    return '<' + str(self.__coords)[1 : -1] + '>'
