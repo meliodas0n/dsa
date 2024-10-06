c64a9cd8dfb96db03ee98a8a00fd3d5e61c1f9db
meliodas0n <mrunalnarayana@gmail.com>
2024-10-06 20:18:15 +0000

    weird algorithm

--- bookscode/main.py
+++ bookscode/main.py
@@ -1,5 +1,14 @@
+def weird_algorithm():
+  n = int(input())
+  while True:
+    print(n, end=" ")
+    if n == 1: break
+    if n % 2 == 0: n = int(n / 2)
+    else: n = int(3 * n + 1)
+  print()
+
 def main():
   print("On a Second thought, I am back to python instead")
 
 if __name__ == "__main__":
-  main()
+  weird_algorithm()
