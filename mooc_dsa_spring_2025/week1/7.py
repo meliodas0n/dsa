6bc2333339a17415648a927a11cc27ab53b55efd
meliodas0n <mrunalnarayana@gmail.com>
2025-02-02 10:31:00 +0000

    what am I missing ?

--- mooc_dsa_spring_2025/week1/7.py
+++ mooc_dsa_spring_2025/week1/7.py
@@ -11,10 +11,38 @@ Your class will be tested using many different grids. The height and width of ea
 
 class WordFinder:
   def set_grid(self, grid):
-    pass
+    self._original = grid
+    self._grid = [[j for j in i] for i in grid]
 
   def count(self, word):
-    pass
+    n = len(word)
+    # horizontal
+    c = 0
+    horizontal_string = "".join(self._original)
+    for i in range(0, len(horizontal_string) - n):
+      if word == horizontal_string[i:i+n]:
+        c += 1
+        
+    # vertical
+    verts = {k: "" for k in range(len(self._grid[0]))}
+    for i in range(len(self._grid[0])):
+      for j in range(len(self._grid)):
+        verts[i] = "".join(verts[i] + self._grid[j][i])
+    for i in verts.values():
+      if i == word:
+        c += 1
+    
+    # diagonal
+    diagonal_string = {k: "" for k in range(2)}
+    if len(self._grid[0]) == len(self._grid):
+      for i in range(len(self._grid)):
+        diagonal_string[0] = "".join(diagonal_string[0] + self._grid[i][i])
+        diagonal_string[1] = "".join(diagonal_string[1] + self._grid[i][len(self._grid[0]) - 1 - i])
+    for diag in diagonal_string.values():
+      for i in range(0, len(diag) - n):
+        if word == horizontal_string[i:i+n]:
+          c += 1
+    return c
 
 if __name__ == "__main__":
   grid = ["TIRATIRA",
