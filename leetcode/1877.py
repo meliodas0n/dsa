4d9a259361126e5cdaa370605e20e9f571e78c31
meliodas0n <mrunalnarayana@gmail.com>
2025-05-27 23:36:56 +0530

    i dont know how to solve this

---
+++ leetcode/1877.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def minPairSum(self, nums: List[int]) -> int:
+        pairs = []
+        for i in range(0, len(nums) - 1, 2):
+            print(i)
+            pairs.append((nums[i], nums[i + 1]))
+
+        # from itertools import pairwise
+        # pairs = pairwise(nums)
+        # for i, pair in enumerate(pairs):
+        #     x, y = pair[0], pair[1]
+        #     for j, pair2 in enumerate(list(pairs[i:])):
+        #         if x == pair2[0] or y == pair2[1]:
+        #             pairs.pop(j)
+
+        print([x for x in pairs])
+
+if __name__ == "__main__":
+    Solution().minPairSum(nums=[3, 5, 2, 3])
